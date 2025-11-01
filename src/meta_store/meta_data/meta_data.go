4371599f77d1244f040b4e5216230986492e6a98
WatchJani <jankokondic84@gmail.com>
2025-11-01 17:13:40 +0100

    stats

--- src/meta_store/meta_data/meta_data.go
+++ src/meta_store/meta_data/meta_data.go
@@ -1,6 +1,11 @@
 package meta_data
 
-import "time"
+import (
+	"path"
+	"time"
+)
+
+const TABLE_PATH string = "metastore/tables"
 
 type Metadata struct {
 	Database         string    `json:"database"`
@@ -13,3 +18,15 @@ type Metadata struct {
 	Version          int       `json:"version"`
 }
 
+func NewMetadata(Database, Table, Owner string, PartitionColumns []string) *Metadata {
+	return &Metadata{
+		Database:         Database,
+		Table:            Table,
+		Location:         path.Join(TABLE_PATH, Database, Table),
+		Format:           "parquet",
+		PartitionColumns: PartitionColumns,
+		CreatedAt:        time.Now(),
+		Owner:            Owner,
+		Version:          1,
+	}
+}
